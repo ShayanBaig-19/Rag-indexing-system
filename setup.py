0bc3987d6cc7aa8f2c7bb07d89fe7cbd0d1d7b5c
shayanbaigminecraft-max <shayanbaigminecraft@gmail.com>
2026-07-13 04:30:43 +0500

    data added and experiment stated in ipynb

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="rag_indexing_system",
+    version="0.0.1",
+    author="Shayan Baig",
+    packages=find_packages(),
+    install_requires=[
+        "pypdf"
+        "langchain",
+        "langchain-text-splitters",
+        "langchain-mistralai",
+        "python-dotenv",
+        "pinecone",
+        "ipykernel",
+    ]
+)
